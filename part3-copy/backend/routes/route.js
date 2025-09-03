74e2583d8e0e057b387f65e193fa15ce21106d94
ganesh-786 <trump72np@gmail.com>
2025-09-03 13:30:43 +0545

    part3-copy was removed at Github

--- part3-copy/backend/routes/route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createUser, getUser } from "../controllers/controller.js";
-const route = express.Router();
-
-route.get("/", getUser);
-route.post("/", createUser);
-
-export default route;
