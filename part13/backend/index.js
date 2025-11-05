f96ae3d73be9afd42ab083e6443cb1919ebdd102
ganesh-786 <trump72np@gmail.com>
2025-11-05 13:19:33 +0545

    ex13.4: folder structured(improved)

--- part13/backend/index.js
+++ part13/backend/index.js
@@ -1,85 +1,18 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
+
+const { connectToDatabase } = require("./utils/db");
+const blogsRouter = require("./controllers/blogs");
+const { PORT } = require("./utils/config");
+
 app.use(express.json());
+app.use("/api/blogs", blogsRouter);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}/api/blogs`);
+  });
+};
 
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  }
-);
-
-Blog.sync();
-
-app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
-});
-
-app.get("/api/blogs/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    res.json(blog);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.post("/api/blogs", async (req, res) => {
-  console.log(req.body);
-  const blog = Blog.build(req.body);
-  blog.important = true;
-  await blog.save();
-  res.json(blog);
-});
-
-app.delete("/api/blogs/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    res.status(200).json({ message: "Successfully Delete" }, blog);
-  } else {
-    res.status(404).end();
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}/api/blogs`);
-});
+start();
---
+++ part13/backend/models/blog.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../utils/db");
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.STRING,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
